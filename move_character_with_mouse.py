68cefd75fe5f227e7cb1c097000cdc5d3912c096
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-10-03 20:31:48 +0900

    Seperate place_hand_randomly(), find_hand(), run_away_hand() / implement place_hand_randomly()

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,28 +1,40 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1000, 800
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-mouse = load_image('hand_arrow.png')
+hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            running = False;
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+                running = False
     pass
 
+def place_hand_randomly():
+    global hand, hand_x ,hand_y
+    hand.draw(hand_x, hand_y)
+    hand_x = random.randint(100, 900)
+    hand_y = random.randint(100, 700)
+    pass
 
+def find_hand():
+    global frame, x, y
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    pass
+
+def run_away_hand():
+    pass
 
 running = True
+hand_x, hand_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
@@ -30,10 +42,12 @@ hide_cursor()
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    mouse.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    place_hand_randomly()
+    find_hand()
+    run_away_hand()
     update_canvas()
     frame = (frame + 1) % 8
+    delay(0.5)
 
     handle_events()
 
